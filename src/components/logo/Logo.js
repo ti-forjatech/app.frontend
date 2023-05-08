bc016523db4de49928ebadf300fd01e25c35ec93
Thyéz <thyezoliveira@gmail.com>
2023-05-07 21:39:13 -0300

    1.5.8

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -3,11 +3,15 @@ import { Canvas } from '@react-three/fiber';
 import { OrthographicCamera } from "@react-three/drei";
 import './logo.css'
 import Honeycomb from './Honeycomb';
+import { motion } from 'framer-motion-3d';
 
 export default function Logo() {
     return (
         <div className="logoWrapper">
-            <div className="logoContainer">
+            <motion.div className="logoContainer"
+            initial={{opacity:0, transform:'translateX(-200px)'}}
+            animate={{opacity:1, transform:'translateX(0px)'}}
+            transition={{delay:1.5, type: 'spring', duration: 1, bounce:true, stiffness:128}}>
                 <Canvas>
                     <OrthographicCamera makeDefault position={[0,0,10]} zoom={20} far={-1} near={1000}>
                         <ambientLight />
@@ -15,8 +19,11 @@ export default function Logo() {
                         <Honeycomb/>
                     </OrthographicCamera>
                 </Canvas>
-                <p className="logoText">forja<span className="logoTextBold">Tech</span></p>
-            </div>
+                <motion.p className="logoText"
+                initial={{transform:'translateY(-50px)'}}
+                animate={{transform:'translateY(0px)'}}
+                transition={{delay:2, type:'spring', duration:1, bounce:true, stiffness:128}}>forja<span className="logoTextBold">Tech</span></motion.p>
+            </motion.div>
         </div>
     );
 }
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,49 +1,64 @@
 import React from 'react';
 import './skills.css';
+import { motion } from 'framer-motion';
 
 export default function Skills(){
+
+    const container = {
+        hidden:{opacity:0},
+        show:{opacity:1,
+        transition: {
+            staggerChildren:.08,
+            type: 'spring'
+        }}
+    }
+    const item = {
+        hidden: {opacity:0, transform: 'translateX(-100px)'},
+        show: {opacity:1, transform: 'translateX(0px)'}
+    }
+
     return (
         <section className="skills">
-            <div className="container">
-                <h1 className="sectionTitle">Resumo Técnico</h1>
-                <div className="titleUnderline"></div>
-                <p className="resume">
+            <motion.div className="container" variants={container} initial="hidden" whileInView="show" viewport={{once: true}}>
+                <motion.h1 className="sectionTitle" variants={item}>Resumo Técnico</motion.h1>
+                <motion.div className="titleUnderline" variants={item}></motion.div>
+                <motion.p className="resume" variants={item}>
                 As principais habilidades, técnicas e ferramentas, que nos permitem realizar nosso trabalho no dia-a-dia.
-                </p>
-                <h2 className="subtitle">Geral</h2>
-                <ul>
+                </motion.p>
+                <motion.h2 className="subtitle" variants={item}>Geral</motion.h2>
+                <motion.ul variants={item}>
                     <li>Gestão de processos</li>
                     <li>Gestão de TI</li>
                     <li>Método de desenvolvimento de software</li>
-                </ul>
-                <h2 className="subtitle">Programação</h2>
-                <ul>
+                </motion.ul>
+                <motion.h2 className="subtitle" variants={item}>Programação</motion.h2>
+                <motion.ul  variants={item}>
                     <li>Python 3.8 e Javascript ES6+</li>
                     <li>Programação OO e Funcional</li>
                     <li>Paradigmas Imperativo e declarativo</li>
                     <li>Padrões de Arquitetura e Projeto</li>
-                </ul>
-                <h2 className="subtitle">Backend</h2>
-                <ul>
+                </motion.ul>
+                <motion.h2 className="subtitle"  variants={item}>Backend</motion.h2>
+                <motion.ul  variants={item}>
                     <li>Flask ou Node.Js</li>
                     <li>Banco de dados MySQL</li>
-                </ul>
-                <h2 className="subtitle">Frontend</h2>
-                <ul>
+                </motion.ul>
+                <motion.h2 className="subtitle" variants={item}>Frontend</motion.h2>
+                <motion.ul variants={item}>
                     <li>Html5 + Sass + JsVanilla.</li>
                     <li>React.Js</li>
-                </ul>
-                <h2 className="subtitle">Nuvem e produção</h2>
-                <ul>
+                </motion.ul>
+                <motion.h2 className="subtitle" variants={item}>Nuvem e produção</motion.h2>
+                <motion.ul variants={item}>
                     <li>AWS Ec2</li>
                     <li>GIT/Github</li>
-                </ul>
-                <h2 className="subtitle">Bibliotecas</h2>
-                <ul>
+                </motion.ul>
+                <motion.h2 className="subtitle" variants={item}>Bibliotecas</motion.h2>
+                <motion.ul variants={item}>
                     <li>Libs do Python e do Node.</li>
                     <li>Framer-motion, Three.js e React-three-fiber e muito mais...</li>
-                </ul>
-            </div>
+                </motion.ul>
+            </motion.div>
         </section>
     )
 }
