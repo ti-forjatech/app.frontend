ea20c7a0769a96c86a308810b7156c96ccc5551c
Thyéz <thyezoliveira@gmail.com>
2023-05-13 18:38:59 -0300

    1.8
    Novo currículo.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './footer.css';
-import curriculo from './Thyéz - curriculo.pdf'
+import curriculo from './Curriculo_Novo_Thyéz_definitivo.pdf';
 
 export default function Footer(){
     return (
         <section className="footer">
             <div className="container">
-                <p className="credit">Site desenvolvido por <a href={curriculo} target="blanc">forjaTech</a></p>
+                <p className="credit">Site desenvolvido por <a href={curriculo} target="blanc">Thyéz</a></p>
             </div>
             <p className="subfooter">forjatech | 2020 - 2023</p>
         </section>
