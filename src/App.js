72fb2298b4f3d4cacd89dafd236134ec47148c23
Thyéz de Oliveira Monteiro (Thyéz) <thyezoliveira@gmail.com>
2023-06-22 14:41:17 +0000

    Update App.jsxx

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-import {useState, useRef } from 'react';
-import Inicio from './components/inicio/Inicio';
-import Sobre from './components/sobre/Sobre';
-import Contato from './components/contato/Contato';
-import Skills from './components/skills/Skills';
-import Footer from './components/footer/Footer';
-import BtnTopo from './components/btnTopo/BtnTopo';
-import Projetos from './components/projetos_/Projetos_';
-import {useInView} from 'framer-motion'
-import './App.css';
+import { useState, useRef } from "react";
+import Inicio from "./components/inicio/Inicio";
+import Sobre from "./components/sobre/Sobre";
+import Contato from "./components/contato/Contato";
+import Skills from "./components/skills/Skills";
+import Footer from "./components/footer/Footer";
+import BtnTopo from "./components/btnTopo/BtnTopo";
+import Projetos from "./components/projetos_/Projetos_";
+import { useInView } from "framer-motion";
+import "./App.css";
 
-//qqq
+//qq
 function App() {
-  const container = useRef(null)
-  const refSobre = useRef(null)
-  const isInView = useInView(
-    refSobre,
-    {
-      margin: "0px 0px -100px 0px",
-      once:1
-    }
-  )
+  const container = useRef(null);
+  const refSobre = useRef(null);
+  const isInView = useInView(refSobre, {
+    margin: "0px 0px -100px 0px",
+    once: 1,
+  });
 
-  const [page, setPage] = useState('landpage')
+  const [page, setPage] = useState("landpage");
 
   return (
     <div className="App" ref={container}>
-      {page === 'landpage' ? 
-      <>
-        <Inicio />
-        <Sobre setPage={setPage} refSobre={refSobre} isInView={isInView}/>
-        <Skills />
-        <Contato />
-        <Footer />
-        <BtnTopo />
-      </>
-       : 
-       <Projetos setPage={setPage}/>}
+      {page === "landpage" ? (
+        <>
+          <Inicio />
+          <Sobre setPage={setPage} refSobre={refSobre} isInView={isInView} />
+          <Skills />
+          <Contato />
+          <Footer />
+          <BtnTopo />
+        </>
+      ) : (
+        <Projetos setPage={setPage} />
+      )}
     </div>
   );
 }
